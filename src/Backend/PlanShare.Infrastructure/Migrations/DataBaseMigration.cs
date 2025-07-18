4e66055ea745ff67106a09ee2e47c1ea57ba3de7
José Jorge Falasco <jose.falasco@datasystem.com.br>
2025-07-18 14:11:04 -0300

    Add project files.

---
+++ src/Backend/PlanShare.Infrastructure/Migrations/DataBaseMigration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PlanShare.Infrastructure.DataAccess;
+
+namespace PlanShare.Infrastructure.Migrations
+{
+    public static class DataBaseMigration
+    {
+        public async static Task MigrationDatabase(IServiceProvider serviceProvider)
+        {
+            var dbContext = serviceProvider.GetRequiredService<PlanShareDbContext>();
+
+            await dbContext.Database.MigrateAsync();
+        }
+    }
+}
